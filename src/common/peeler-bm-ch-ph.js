7558bc7baf4b77726525cf34d7715acfe28c1206
Marcel Duran <contact@marcelduran.com>
2012-03-15 16:06:07 -0700

    reusing code

--- src/common/peeler-bm-ch-ph.js
+++ src/common/peeler-bm-ch-ph.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2012, Yahoo! Inc.  All rights reserved.
+ * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
+ */
+
 /*global YSLOW*/
 
 YSLOW.peeler.peel = function (node) {
