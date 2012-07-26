da3a53c21dbb9651e967a74a9e96a720796a0050
marcelduran <marcelduran@gmail.com>
2012-07-25 22:08:39 -0700

    new release/firefox issue 49 fix

--- src/firefox/chrome/content/yslow-firefox.js
+++ src/firefox/chrome/content/yslow-firefox.js
@@ -75,36 +75,14 @@ YSLOW.firefox = {
     },
 
     startup: function (wmode) {
-        var i,
-            prefs = YSLOW.util.Preference.getPrefList("extensions.firebug.yslow.");
-
         this.windowMode = wmode;
 
-        // Preference branch has been changed starting YSlow2, copy over the preference from YSlow1
-        if (prefs && prefs.length > 0) {
-            for (i = 0; i < prefs.length; i += 1) {
-                if (YSLOW.util.Preference.getPref(prefs[i].name) !== undefined) {
-                    YSLOW.util.Preference.setPref(prefs[i].name, prefs[i].value);
-                }
-                // delete old pref
-                YSLOW.util.Preference.deletePref("extensions.firebug.yslow." + prefs[i].name);
-            }
-        }
-
         if (typeof Firebug !== 'undefined') {
-            // PATCH by Adrian Yee (adrian@gossamer-threads.com) GTMetrix
-            // via exceptional-performance@yahoogroups.com
-            //
-            // There seems to be some sort of timing issue with YSlow and
-            // Firebug which causes an error in YSLOW.FBYSlow.init(),
-            // "YSLOW.firefox.startup():TypeError: FBL.extend is not a
-            // function".  Hack around this by retrying the call to
-            // YSLOW.FBYSlow.init() if it fails.
             if (typeof FBL === 'undefined' || (typeof FBL !== 'undefined' &&
                     typeof FBL.extend !== 'function')) {
                 YSLOW.util.setTimer(function() {
                     YSLOW.firefox.startup(wmode);
-                }, 10);
+                }, 100);
                 return;
             }
 
@@ -123,7 +101,7 @@ YSLOW.firefox = {
         } else {
             YSLOW.util.setTimer(function() {
                 YSLOW.firefox.startup(wmode);
-            }, 10);
+            }, 100);
             return;
         }
 
